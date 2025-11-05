212b14bd4c3431ef91ed9409b20f67e2fcc192a4
Daniel Lagally <daniel.lagally@gmail.com>
2025-11-05 22:56:58 +0100

    add a toplevel flake to avoid ugly url syntax

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Templates and devShells for courses at TUM";
+
+  inputs = {
+    flake-utils.url =  "github:numtide/flake-utils"; # TODO
+
+    gbs = { url = "./gbs"; };
+    it-sec = { url = "./it-sec"; };
+  };
+
+  outputs = { self, flake-utils, ... } @ inputs :
+  let
+    system = "x86_64-linux"; # don't fixiate this
+    courses = {
+      gbs = inputs.gbs;
+      it-sec = inputs.it-sec;
+    };
+
+    build_templates = builtins.mapAttrs (name: value: {
+      path = ./${name};
+      description = "${name} template";
+      welcomeText = "Please allow direnv by typing 'direnv allow'";
+    });
+    templates = build_templates courses;
+
+    build_devShells = courses: {${system} = builtins.mapAttrs (name: value: courses.${name}.devShells.x86_64-linux.default) courses;};
+    devShells = build_devShells courses;
+  in
+    {
+      inherit templates;
+      inherit devShells;
+    };
+}
